31f14963c4ac2b4ead8c1afa1be8eb22a207de5c
holgermueller <9jholger@gmail.com>
2024-04-13 16:00:45 -0500

    finished desktop layout

--- submit.js
+++ submit.js
@@ -9,16 +9,11 @@ const checkEmail = (event) => {
 
   if (result == false) {
     document.getElementById("messages").innerHTML =
-      "Please provide a valid email";
+      "Please provide a valid email address";
+    document.getElementById("messages").style.marginTop = "2%";
     document.getElementById("messages").style.display = "block";
     document.getElementById("messages").style.color = "red";
-
     document.getElementById("email").style.borderColor = "red";
-    document.getElementById("email").style.backgroundImage =
-      "url(images/icon-error.svg)";
-    document.getElementById("email").style.backgroundRepeat = "no-repeat";
-    document.getElementById("email").style.backgroundPosition = "scroll";
-    document.getElementById("email").style.backgroundPosition = "85%";
   } else {
     document.getElementById("messages").innerHTML = "Thank you!";
     document.getElementById("messages").style.display = "block";
